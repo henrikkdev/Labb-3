5b2843a67d351d1e5829b764ce9f8b69185c5d2e
Linus Backlund <linus.hbg@hotmail.com>
2024-12-05 15:50:27 +0100

    Meny 1

--- Labb 3/Program.cs
+++ Labb 3/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("***************************************************************************\nHello and welcome to the TemperatureCalculator!\n***************************************************************************");
+
+            while (true) {
+
+                Console.WriteLine("***************************************************************************\n\nType 1 if you want to continiue \nType 2 if you want to exit\n");
+                Console.Write("Your input: ");
+                string UserInput = Console.ReadLine();
+                Console.WriteLine("\n***************************************************************************\n");
+
+                switch(UserInput)
+
+
+                    case "1"{
+
+                    }
+                case "2"{
+                        Console.WriteLine("Thank you for using this program, hope to see you again soon");
+                        return;
+                    }
+                }
+            } 
         }
     }
 }
